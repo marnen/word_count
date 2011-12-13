bbb176c504269a854dd04e91cafaa7fe28336c00
Marnen Laibow-Koser <marnen@marnen.org>
2011-12-13 01:13:39 -0500

    It works, but it's ugly. Time to refactor.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -5,7 +5,9 @@ class WordCount
     sentences.each_with_index do |sentence, i|
       words = sentence.downcase.split(%r{\W+})
       words.each do |word|
-        frequency_table[word] = frequency_table[word].to_a << (i + 1)
+        unless word =~ %r{\A\s*\Z}
+          frequency_table[word] = frequency_table[word].to_a << (i + 1)
+        end
       end
     end
     
--- word_count.rb
+++ word_count.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each {|file| require file }
+
+sentences = []
+ARGF.each('.') {|s| sentences << s }
+
+frequency_table = WordCount.count sentences
+full_table = []
+frequency_table.each {|key, value| full_table << [key, value.length, value.inspect] }
+
+puts full_table.sort_by! {|row| -row[1] }.collect {|row| row.join ', '}
